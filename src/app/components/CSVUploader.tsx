64f79d8b8eb68b2da1b35fd5a5803391f28f393b
Ben Roberts <bendando@hotmail.co.uk>
2023-06-04 11:57:04 +0100

    first commit

---
+++ src/app/components/CSVUploader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CSVUploader = () => {
+    return (
+        <div>
+            <h1 className='font-bold text-2xl'>We want to use drop zone to import and pass the contents of a csv to a backend...</h1>
+
+            <h1>We want to be able to configure which backend we are passing the data to</h1>
+        </div>
+    );
+};
+
+export default CSVUploader;
