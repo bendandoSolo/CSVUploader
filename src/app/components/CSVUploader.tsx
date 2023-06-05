f447657cf2ab967c704da516dbdffcf966feccd2
Ben Roberts <bendando@hotmail.co.uk>
2023-06-05 08:47:23 +0100

    DONE Receiving files

--- src/app/components/CSVUploader.tsx
+++ src/app/components/CSVUploader.tsx
@@ -5,11 +5,34 @@ import {useDropzone} from 'react-dropzone'
 const CSVUploader = () => {
 
     const onDrop = useCallback((acceptedFiles: any) => {
+    //const onDrop = useCallback((acceptedFiles: any) => {
         // Do something with the files
 
        // console.log(acceptedFiles);
-      }, [])
-      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+        alert(acceptedFiles[0].name);
+        alert('one file at a time');
+        
+        acceptedFiles.forEach((file: any) => {
+            const reader = new FileReader()
+    
+            reader.onabort = () => console.log('file reading was aborted')
+            reader.onerror = () => console.log('file reading has failed')
+            reader.onload = () => {
+            // Do whatever you want with the file contents
+            const binaryStr = reader.result
+            console.log(binaryStr)
+             sendData(binaryStr);
+            }
+            reader.readAsText(file)
+        })
+    }, [])
+
+    const sendData = (binaryStr: any) => {
+        alert('we want to send the data to the backend here...and move this to services folder');
+    }
+
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
     return (
         <div>
@@ -18,8 +41,8 @@ const CSVUploader = () => {
             <div {...getRootProps()}>
         <input {...getInputProps()} />
         {
-            isDragActive ?
-            <p>Drop the files here ...</p> :
+            // isDragActive ?
+            // <p>Drop the files here ...</p> :
             <div className='border-red-600 border-4 p-32'> <p>Drag and Drop some files here, or click to select files</p></div>
            
             
@@ -36,3 +59,106 @@ const CSVUploader = () => {
 };
 
 export default CSVUploader;
+
+// TOTAL = 15
+// TOTAL= 35.25			
+// TOTAL = 48.75 		
+// TOTAL = 57.25		
+// TOTAL = 70.25			
+// TOTAL = 77.25 
+//  TOTAL = 85 				
+// TOTAL = 93.5				
+// TOTAL = 115.5 				
+// TOTAL = 125.5 :				
+// TOTAL = 136 				
+//     TOTAL = 146 			
+// TOTAL = 156.5 			
+//     really = 161.5 				
+//     = 196.5					
+//     = 226.5 				
+//     = 253.5 				
+//     = 278.5 					
+//     = 297				
+//     =   306.5				
+//     = 315.5 	
+//     = 323.5 
+//     = 333 				
+//     = 345.5 	
+//     = 357.5 			
+//      = 375 			
+//      = 386.5 			
+//         = 408.5 					
+//           = 433.5hrs					
+//         hours = 464.5 			
+//         TOTAL : 477				
+//         TOTAL 491 				
+//      TOTAL 508.5 	
+
+//      15hrs        
+//      20.25hrs     
+//      13.5 hours   
+//      8.5 hours    
+//      13.5 hours   
+//      7 hours      
+//      7.75 hours   
+//      8.5 hours    
+//      22 hours     
+//      10 hours     
+//      10.5 hours   
+//      10hours      
+//      10.5 hours   
+//      5 hours      
+//      35 hours     
+//      30 hours     
+//      27 hours     
+//      25 hours     
+//      18.5 hours   
+//      9.5 hours    
+//      9 hours      
+//      8 hours      
+//      9.5 hours    
+//     12.5 hours    
+//     12 hours      
+//      17.5 hrs     
+//      11.5 hrs     
+//      22           
+//      25           
+//      31           
+//     12.5 hours    
+//      14hrs        
+//     17.5          
+
+// week 1
+// week 2
+// week 3
+// week 4
+// week 5
+// week 6
+// week 7
+// week 8
+// week 9
+// week 10
+// week 11
+// week 12
+// week 13
+// week 14
+// week 15 
+// week 16 
+// week 17 
+// week 18
+// week 19
+// week 20
+// week 21
+// week 22
+// week 23
+// week 24
+// week 25
+// week 26
+// week 27
+// week 28
+// week 29
+// week 30
+// week 31
+// Week 32
+// Week 33
+
